b61b3bd9bf787e905561dd18e952c1a0946719fd
wangyang <1749748955@qq.com>
2020-11-21 21:26:20 +0800

    update

---
+++ data/raw_data/cmd1_download_sra.sh
@@ -0,0 +1,5 @@
+wget -O SRR1916152.sra https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-5/SRR1916152/SRR1916152.1
+wget -O SRR1916153.sra https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-5/SRR1916153/SRR1916153.1
+wget -O SRR1916154.sra https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-5/SRR1916154/SRR1916154.1
+wget -O SRR1916155.sra https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-5/SRR1916155/SRR1916155.1
+wget -O SRR1916156.sra https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-5/SRR1916156/SRR1916156.1
---
+++ data/raw_data/cmd2_sra2fastq.sh
@@ -0,0 +1,5 @@
+fastq-dump SRR1916152.sra 
+fastq-dump SRR1916153.sra 
+fastq-dump SRR1916154.sra 
+fastq-dump SRR1916155.sra 
+fastq-dump SRR1916156.sra 
